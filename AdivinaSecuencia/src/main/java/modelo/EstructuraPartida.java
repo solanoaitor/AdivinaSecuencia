705da34a2becfa4d0d31b15d5fcb6a8ae9663500
aitorsolano <aitormartin.solano@e-campus.uab.cat>
2021-11-13 11:30:02 +0100

    Comentarios de clase EstructuraPartida
    Comentarios de la clase EstructuraPartida.

--- AdivinaSecuencia/src/main/java/modelo/EstructuraPartida.java
+++ AdivinaSecuencia/src/main/java/modelo/EstructuraPartida.java
@@ -4,19 +4,35 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
-//TODO: reducir complejidad
+/**
+ * @autor       Desirée y Aitor
+ * @name 		EstructuraPartida.java
+ * @description	Clase que contiene toda la lógica del juego, crea la solución, revisa los números e intentos realizados
+ *
+ */
 public class EstructuraPartida {
     private int solucion[];
     private String intentos[][];
     private int intento;
     private Random numeroRandom = new Random();
+
+    /**
+     * @constructor         Constructor amb parámetres intentosMaximos
+     * @param intentosMaximos   Parametros de intentos máximos permitidos
+     * @solucion        Slución final del juego
+     * @intentos        Array de strings donde guardan los intentos introducidos por el usuario
+     * @intento         Intento actual introducido por el usuario
+     */
     public EstructuraPartida(int intentosMaximos) {
         this.solucion = new int[4];
         this.intentos = new String[intentosMaximos][6];
         this.intento = 0;
     }
 
-    //Crea una solucion valida y comprueba que ningun numero es igual al anterior
+    /**
+     * @solucion    Realiza la solución válida para ganar
+     * @return      Devuelve la solución final del juego
+     */
     public int [] solucion(){
         Set<Integer> numerosUsados = new HashSet<>();
         int total = 0;
@@ -30,7 +46,12 @@ public class EstructuraPartida {
         }
         return solucion;
     }
-    //TODO: reducir complejidad
+
+    /**
+     * @revisaNumero    Revisa el número introducido por el usuario es un dígito y lo transforma a un integer
+     * @param valor     Le pasamos el valor en string introducido por el usuario
+     * @return          Devolvemos true o false si es de 4 dígitos o no
+     */
     public boolean revisaNumero(String valor) {
         if(valor.length() != 4) {
             return false;
@@ -43,6 +64,11 @@ public class EstructuraPartida {
         }
     }
 
+    /**
+     * @introduceIntentos   Método para guardar los intentos realizados por el usuario
+     * @param valor         Pasamos el valor introducido por el usuario
+     * @return              Devuelve les intentos realizados por el usuario
+     */
     public String[][] introduceIntentos(String valor) {
         char[] valueSeparate = valor.toCharArray();
         for(int i = 0; i < 4; i++) {
@@ -51,6 +77,12 @@ public class EstructuraPartida {
         return intentos;
     }
 
+    /**
+     * @introduceBolas      Introduce bolas negras y bolas blancas en el array de intentos.
+     * @param bolaNegra     Bolas negras introducidas
+     * @param bolaBlanca    Bolas blancas introducidas
+     * @return      Devuelve todo el array de intentos
+     */
     public String[][] introduceBolas(int bolaNegra, int bolaBlanca) {
         String blanca = String.valueOf(bolaBlanca);
         String negra = String.valueOf(bolaNegra);
@@ -59,18 +91,34 @@ public class EstructuraPartida {
         return intentos;
     }
 
+    /**
+     * @getArraySolucion    Método que devuelve en un array con la solución
+     * @return      Devuelve el array con la solución
+     */
     public int[] getArraySolucion() {
         return solucion;
     }
 
+    /**
+     * @getIntentos Método que devuelve los intentos del usuario
+     * @return      Devuelve un array con los intentos introducidos por el usuario
+     */
     public int getIntentos() {
         return intento;
     }
 
+    /**
+     * @getArrayIntentos    Método para devolver todos los intentos de el usuario
+     * @return  Devuelve todos los intentos intriducidos por el usuario
+     */
     public String[][] getArrayIntentos() {
         return intentos;
     }
 
+    /**
+     * @setIntentos     Método para introducir los intentos del usuairo
+     * @param intento   Introduce el intento del usuario
+     */
     public void setIntentos(int intento) {
         this.intento = intento;
     }
